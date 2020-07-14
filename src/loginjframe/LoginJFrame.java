31e07541d51b666df5d00dcd63f6789fd733036f
UserDELL <UserDELL@192.168.200.30>
2020-07-14 17:47:43 -0500

    Login JFrame con java netbenas

---
+++ src/loginjframe/LoginJFrame.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loginjframe;
+
+/**
+ *
+ * @author UserDELL
+ */
+public class LoginJFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        IniciarSesion i = new IniciarSesion();
+        i.setVisible(true);
+        i.setSize(310,490);        
+    }
+    
+}
